da0ad79f21e46eb0e66b51331d3ddb99390eba7f
AngeliqueVanVeen <38880437+AngeliqueVanVeen@users.noreply.github.com>
2018-06-14 11:32:49 +0200

    Invoer wordt weggeschreven naar .txt
    De eigenaar van het blog kan invoervelden invullen. Die info wordt dan weggeschreven in een .txt bestand.

---
+++ toevoegen.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BLOG - Toevoegen</title>
+  </head>
+  <body>
+    <h1>Op deze pagina blogs toevoegen</h1><br>
+
+    <h3>Blog formulier:</h3><br>
+
+    <form id="login" action="blog.php" method="POST">
+
+      Blogtitel:<br>
+      <input type="text" name="blogtitel"><br><br>
+      Naam:<br>
+      <input type="text" name="naam"><br><br>
+
+      Tekst invoer:<br>
+      <textarea id="blogtekstinvoer" name="blogtekst" cols=70 rows=10></textarea><br><br><br>
+
+      <input type="submit" value="Verzenden">
+
+    </form>
+
+  </body>
+</html>
